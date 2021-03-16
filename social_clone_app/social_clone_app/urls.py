d0643022c094ca828569724701dea2b77ee73114
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-16 11:22:51 +0530

    check commit

--- social_clone_app/social_clone_app/urls.py
+++ social_clone_app/social_clone_app/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
 from account.views import UserProfile
 
 urlpatterns = [
